9919ca1be40dde9803d8dbd8a1feab2b9c0b3f1f
KingFredrickVI <email@example.com>
2017-04-21 15:35:10 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+  name = 'vidme',
+  packages = ['vidme'], # this must be the same as the name above
+  version = '0.8.3',
+  description = 'This is a Python VidMe API interface library.',
+  author = 'KingFredrickVI',
+  author_email = 'jakewright458@gmail.com',
+  url = 'https://github.com/KingFredrickVI/VidMeAPI', # use the URL to the github repo
+  download_url = 'https://github.com/KingFredrickVI/VidMeAPI/archive/master.zip', # I'll explain this in a second
+  keywords = ['vidme', 'VidMe', 'Vid.Me', 'vid.me', 'python', 'api', 'vidme api'], # arbitrary keywords
+  classifiers = [],
+)
+
+# setuppy sdist upload -r pypi
